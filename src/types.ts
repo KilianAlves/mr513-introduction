2edf4bf863bf517b26e7904b7a7396c10061f10d
KilianAlves <alveskilian@gmail.com>
2023-09-06 14:12:09 +0200

    Fin de la partie multiplication de vecteur

--- src/types.ts
+++ src/types.ts
@@ -22,4 +22,21 @@ function isPalindrome(word: string): boolean {
 console.log("isPalindrome ---------------------------------------------------");
 console.log(isPalindrome('radar')); // doit afficher true
 console.log(isPalindrome('abca')); // doit afficher false
+
+
+// Ajoutez les annotations de type à la déclaration de la fonction suivante et écrivez son code :
+
+type Vector2D = {
+    x: number;
+    y: number;
+}
+
+function dotProduct(vector1: Vector2D, vector2: Vector2D): number {
+    let ResX: number = vector1.x * vector2.x;
+    let ResY: number = vector1.y * vector2.y;
+    return ResX + ResY;
+}
+
+console.log("dotProduct ---------------------------------------------------");
+console.log(dotProduct({ x : 1, y : 2 }, { x : 4, y : 5 })); // doit afficher 14
         
