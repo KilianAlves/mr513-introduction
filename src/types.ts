c943711f5261cc152a4380c97d1b03e72d255be4
KilianAlves <alveskilian@gmail.com>
2023-09-06 14:05:42 +0200

    Fin du 3.3

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,21 @@ let lastName: string = "Dupont";
 let age: number = 23;
 let isStudent: boolean = true;
 let marks: number[] = [ 10, 15.5, 8 ];
+
+
+// Ajoutez les annotations de type à la déclaration de la fonction suivante et écrivez son code :
+
+function isPalindrome(word: string): boolean {
+
+    if(word == word.split('').reverse().join('')) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log("isPalindrome ---------------------------------------------------");
+console.log(isPalindrome('radar')); // doit afficher true
+console.log(isPalindrome('abca')); // doit afficher false
+        
