e8c8033f7d82bbb84f8090c5b65f39e7e0aa68ca
KilianAlves <alveskilian@gmail.com>
2023-09-06 14:37:11 +0200

    Ajout de la classe Shape, classe abstraite et de classe Rectangle Square et Circle

--- src/types.ts
+++ src/types.ts
@@ -40,3 +40,57 @@ function dotProduct(vector1: Vector2D, vector2: Vector2D): number {
 console.log("dotProduct ---------------------------------------------------");
 console.log(dotProduct({ x : 1, y : 2 }, { x : 4, y : 5 })); // doit afficher 14
         
+
+interface Shape {
+    readonly name: string;
+    readonly isPolygon: boolean;
+    getArea(): number;
+}
+
+abstract class AbstractShape implements Shape{
+    readonly name: string;
+    readonly isPolygon: boolean;
+
+    constructor(name: string, isPolygon: boolean) {
+        this.name = name;
+        this.isPolygon = isPolygon;
+    }
+    abstract getArea(): number;
+}
+
+class Rectangle  extends AbstractShape{
+    side1: number;
+    side2: number;
+
+    constructor(name: string, side1: number, side2: number) {
+        super(name,true);
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    getArea(): number {
+        return this.side1 * this.side2;
+    }
+}
+
+class Square extends Rectangle{
+    constructor(name: string, side: number) {
+        super(name,side, side)
+    }
+}
+
+class Circle extends AbstractShape{
+    radius: number;
+
+    constructor(name: string, radius: number) {
+        super(name,false);
+        this.radius = radius;
+    }
+
+    getArea(): number {
+        return this.radius* Math.PI;
+    }
+}
+
+
+
