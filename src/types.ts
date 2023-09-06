5fa9966a272ffa5a6c54f80c62602b34f89c0b24
KilianAlves <alveskilian@gmail.com>
2023-09-06 13:59:58 +0200

    Fin du 3.2

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+
+// Ajoutez les annotations de types pour les variables suivantes :
+
+let lastName: string = "Dupont";
+let age: number = 23;
+let isStudent: boolean = true;
+let marks: number[] = [ 10, 15.5, 8 ];
