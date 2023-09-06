64ed870d1dc3cac44cb6c4a2df264a5d15f196c5
KilianAlves <alveskilian@gmail.com>
2023-09-06 13:54:24 +0200

    Fin du 2.1

---
+++ src/intro.ts
@@ -0,0 +1,7 @@
+console.log("--------------------------------------------------- addNumbers ");
+
+    function addNumbers(x, y) {
+        return x + y;
+    }
+
+    console.log(addNumbers(3, 4));
