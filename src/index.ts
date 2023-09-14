6c0b7456657a7aabc50eeea12018620a6d0ff0f5
KilianAlves <alveskilian@gmail.com>
2023-09-14 12:05:38 +0200

    Fin de la partie création de vue avec EJS

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config()
+
+const port = process.env.port;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/:title', (req, res) => {
+    res.render('index', { title: req.params.title });
+});
+
+app.listen(port, () => {
+    console.log(`Server local démarré : http://localhost:${port}`);
+});
