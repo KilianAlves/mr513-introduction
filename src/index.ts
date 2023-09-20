5612b5027b8d10e7e7d39ba37f9a42cae1df009c
KilianAlves <alveskilian@gmail.com>
2023-09-20 15:26:09 +0200

    Fin de seance

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import express from 'express';
 import { Game } from './game/game.model';
+import { body } from 'express-validator';
+import { validationResult } from 'express-validator';
+var session = require('express-session')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config()
 
 const port = process.env.port;
 
 const app = express();
+
+app.use(session({
+    secret: process.env.session_secret, // ajoutez la variable d'environnement correspondante au fichier .env
+    saveUninitialized: false,
+    resave: false
+}));
+
+declare module "express-session" {
+    interface SessionData {
+        game: Game;
+    }
+}
 
 app.set('view engine', 'ejs');
 
@@ -18,6 +33,17 @@ app.use(express.static('public'));
 app.get('/play', (req, res) => {
     res.render('game/play', { game: new Game });
 });
+
+app.post('/play',
+        express.urlencoded({ extended: true }),
+        body('guess').isInt({ min: 1, max: 1000 }),
+        (req, res) => {
+             const result = validationResult(req);
+            if (result.isEmpty()) {
+                return res.send(`Hello!`);
+            }
+            console.log("test");
+    });
 
 app.listen(port, () => {
     console.log(`Server local démarré : http://localhost:${port}`);
